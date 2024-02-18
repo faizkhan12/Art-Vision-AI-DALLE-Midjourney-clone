4fa34f686ca002ecd3cd178ec6ee8d8b1b45a323
faizkhan12 <faizk6797@gmail.com>
2024-02-18 12:27:34 +0530

    updrade DALL-E v3 and make some configuration changes

--- server/routes/dalle.js
+++ server/routes/dalle.js
@@ -19,14 +19,14 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
+      model: "dall-e-3",
       prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
-
-    const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse.data;
     res.status(200).json({ photo: image });
   } catch (error) {
     console.log(error);
